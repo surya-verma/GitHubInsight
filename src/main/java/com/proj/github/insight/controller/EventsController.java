694b7f722c20a6f46ad17c2430ce21af058b1c81
suryaverma <suryakv14@outlook.com>
2020-06-09 22:06:33 -0700

    Fix

--- src/main/java/com/proj/github/insight/controller/EventsController.java
+++ src/main/java/com/proj/github/insight/controller/EventsController.java
@@ -2,6 +2,7 @@ package com.proj.github.insight.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class EventsController {
 	@Autowired
 	EventService eventService;
 	
-    @RequestMapping("/repos/{userName}/{repoName}/events")
+    @GetMapping("/repos/{userName}/{repoName}/events")
     public ResponseEntity<?> getEventsByUserAndRepo(@PathVariable("userName") String userName,@PathVariable("repoName") String repoName){
         return eventService.getEventsByUserAndRepo(userName, repoName);
     }
